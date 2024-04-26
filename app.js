5b9ded51196a3ce0ddb38c19f0e7b31d57363880
hwuiinn <gnldls8406@gmail.com>
2024-04-26 20:52:16 +0900

    Study : Express.js 설치 및 미들웨어 개념 학습

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const http = require("http");
-const routes = require("./routes");
+const express = require("express");
 
-const server = http.createServer(routes);
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("이건 미들웨어입니다.");
+  console.log(111, req.url);
+  console.log(222, req.method);
+  next(); // next()를 사용하여 바로 아래에 있는 미들웨어를 실행시킬 수 있다.
+});
+
+app.use((req, res, next) => {
+  console.log("이건 다른 미들웨어2 입니다.");
+  console.log(333, req.path);
+});
+
+const server = http.createServer(app);
 
 server.listen(3100);
