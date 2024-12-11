39e296b8ac155e5e5b9e46a12f3f656021466c6f
hwuiinn <gnldls8406@gmail.com>
2024-12-11 23:49:43 +0900

    feat: mysql connect to node.js

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const errorController = require('./controllers/error');
+const errorController = require("./controllers/error");
+const db = require("./util/database");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+db.execute(); // execute 내부에 sql문을 작성하여 쿼리 결과 조회 가능
+
+const { route } = require("./routes/admin");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
